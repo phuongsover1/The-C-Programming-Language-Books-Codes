47d9927f7af0f156910610b20aae899ec14cfc19
Phuong Nguyen <phuongsover2@gmail.com>
2023-08-26 12:43:16 +0700

    1.5.2 Character Counting

---
+++ chapter-1/1.5-file-copying-2nd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* copy input to output; 2nd version */
+int main() {
+  int c;
+
+  c = getchar();
+  while ((c = getchar()) != EOF) {
+    putchar(c);
+  }
+
+  return 0;
+}
