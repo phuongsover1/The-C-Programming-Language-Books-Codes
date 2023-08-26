4ea6ffb4adbb8b650013a7e9abc719221efe6e32
Phuong Nguyen <phuongsover2@gmail.com>
2023-08-26 12:38:19 +0700

    watch long size

--- chapter-1/print-sizeof.c
+++ chapter-1/print-sizeof.c
@@ -3,6 +3,7 @@
 int main() {
   printf("size of short: %zu\n", sizeof(short));
   printf("size of int: %zu\n", sizeof(int));
+  printf("size of long: %zu\n", sizeof(long));
 
   int myArray[10];
   printf("Size of myArray: %zu\n", sizeof(myArray));
