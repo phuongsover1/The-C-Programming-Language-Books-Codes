3f11882054c18654a3e6f28be2200ef005737c20
Phuong Nguyen <phuongsover2@gmail.com>
2023-08-26 12:37:57 +0700

    complete exercises 1-6, 1-7

---
+++ chapter-1/exercises/exercise_1-6.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("value of expression: %d\n", getchar() != EOF);
+  return 0;
+}
---
+++ chapter-1/exercises/exercise_1-7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Value of EOF: %d", EOF);
+  return 0;
+}
